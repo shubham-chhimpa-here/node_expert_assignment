99c91f0ce827b0397ae72941fb5ba023883890c3
Shubham Chhimpa <shubhamchhimpa2001@gmail.com>
2024-03-17 19:52:05 +0530

    submit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react"
+
 export default function Navbar() {
+    const [state, setState] = useState(false)
     return <>
         <header className="p-4 bg-violet-400 text-gray-800 ">
             <div className="container flex justify-between h-16 mx-auto max-w-8xl">
@@ -7,6 +10,7 @@ export default function Navbar() {
                         <path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
                     </svg>
                     <span className="ml-1 text-2xl font-semibold text-white">Learning Exp.</span>
+
                 </a>
 
                 <div className="items-center flex-shrink-0 hidden lg:flex">
